ac7c9dabb40acb22510da2758c6dd8d978ba6d8f
kruntarat <kruntarat_sa@kkumail.com>
2023-11-22 22:59:19 +0700

    add new index

--- search.php
+++ search.php
@@ -47,6 +47,15 @@ $result = $conn->query($sql);
     <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"></script>
+
+    <link rel="shortcut icon" href="./assets/img/logo (3).svg">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900;1,400;1,500;1,700&display=swap"rel="stylesheet">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/feather.css">
+    <link rel="stylesheet" href="./css/flags.css">
+    <link rel="stylesheet" href="./css/fontawesome.min.css">
+    <link rel="stylesheet" href="./assets/plugins/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="./css/style.css">
     <script>
     $(document).ready(function() {
         $("#advisor_name").autocomplete({
@@ -64,11 +73,7 @@ $result = $conn->query($sql);
         }
     });
     </script>
-    <style>
-    body {
-        margin: 0 5em;
-    }
-    </style>
+
 </head>
 
 <body>
@@ -180,6 +185,127 @@ $result = $conn->query($sql);
         </tbody>
     </table>
 
+    <div class="main-wrapper">
+        <div class="page-wrapper">
+            <div class="content container-fluid">
+
+                <div class="row">
+                    <div class="col-xl-3 col-sm-6 col-12 d-flex">
+                        <div class="card bg-comman w-100">
+                            <div class="card-body">
+                                <div class="db-widgets d-flex justify-content-between align-items-center">
+                                    <div class="db-info">
+                                        <h6>Students</h6>
+                                        <h3>50055</h3>
+                                    </div>
+                                    <div class="db-icon">
+                                        <img src="./assets/img/icons/dash-icon-01.svg" alt="Dashboard Icon">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-sm-6 col-12 d-flex">
+                        <div class="card bg-comman w-100">
+                            <div class="card-body">
+                                <div class="db-widgets d-flex justify-content-between align-items-center">
+                                    <div class="db-info">
+                                        <h6>Awards</h6>
+                                        <h3>50+</h3>
+                                    </div>
+                                    <div class="db-icon">
+                                        <img src="./assets/img/icons/dash-icon-02.svg" alt="Dashboard Icon">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-sm-6 col-12 d-flex">
+                        <div class="card bg-comman w-100">
+                            <div class="card-body">
+                                <div class="db-widgets d-flex justify-content-between align-items-center">
+                                    <div class="db-info">
+                                        <h6>Department</h6>
+                                        <h3>30+</h3>
+                                    </div>
+                                    <div class="db-icon">
+                                        <img src="./assets/img/icons/dash-icon-03.svg" alt="Dashboard Icon">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-sm-6 col-12 d-flex">
+                        <div class="card bg-comman w-100">
+                            <div class="card-body">
+                                <div class="db-widgets d-flex justify-content-between align-items-center">
+                                    <div class="db-info">
+                                        <h6>Revenue</h6>
+                                        <h3>$505</h3>
+                                    </div>
+                                    <div class="db-icon">
+                                        <img src="./assets/img/icons/dash-icon-04.svg" alt="Dashboard Icon">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12 col-lg-6">
+
+                        <div class="card card-chart">
+                            <div class="card-header">
+                                <div class="row align-items-center">
+                                    <div class="col-6">
+                                        <h5 class="card-title">Publications</h5>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div id="apexcharts-area"></div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="col-md-12 col-lg-6">
+
+                        <div class="card card-chart">
+                            <div class="card-header">
+                                <div class="row align-items-center">
+                                    <div class="col-6">
+                                        <h5 class="card-title">Number of Students</h5>
+                                    </div>
+                                    <div class="col-6">
+                                        <ul class="chart-list-out">
+                                            <li><span class="circle-blue"></span>Girls</li>
+                                            <li><span class="circle-green"></span>Boys</li>
+                                            <li class="star-menus"><a href="javascript:;"><i
+                                                        class="fas fa-ellipsis-v"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                <div id="bar"></div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="./assets/js/jquery-3.6.0.min.js"></script>
+    <script src="./assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="./assets/js/feather.min.js"></script>
+    <script src="./assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+    <script src="./assets/plugins/apexchart/apexcharts.min.js"></script>
+    <script src="./assets/plugins/apexchart/chart-data.js"></script>
+    <script src="./js/script.js"></script>
 </body>
 
 </html>
