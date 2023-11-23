77434cd01106855dd8def2869df7be88950aaa8a
Sutinun-p <sutinun.p@kkumail.com>
2023-11-23 20:22:50 +0700

    first commit

---
+++ assets/js/search.js
@@ -0,0 +1,16 @@
+const optionMenu = document.querySelector(".select-menu"),
+  selectBtn = document.querySelector(".select-btn"),
+  option = document.querySelectorAll(".option"),
+  sBtn_text = document.querySelector(".sBtn-text");
+
+selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"));
+
+option.forEach(option =>{
+    option.addEventListener("click",()=>{
+        let selectedOption = option.querySelector(".option-text").innerText;
+        sBtn_text.innerText = selectedOption;
+
+        optionMenu.classList.remove("active");
+    })
+ 
+})
