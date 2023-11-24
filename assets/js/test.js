b7fb73500ee0293af914ffb1a4614185b48b72f5
Sutinun-p <sutinun.p@kkumail.com>
2023-11-24 16:36:48 +0700

    codenew

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,49 +1,43 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var showAllButton = document.getElementById('showAllAuthors');
-    
-    showAllButton.addEventListener('click', function() {
-        // Logic to show all authors
-        alert('Show all authors clicked!');
+document.addEventListener("DOMContentLoaded", () => {
+  // Function to initialize each dropdown
+  function setupDropdown(toggleButtonId, dropdownMenuId) {
+    const toggleButton = document.getElementById(toggleButtonId);
+    const dropdownMenu = document.getElementById(dropdownMenuId);
+
+    // Toggle the dropdown
+    toggleButton.addEventListener("click", () => {
+      const isShown = dropdownMenu.style.display === "block";
+      // Hide any other open dropdowns
+      document.querySelectorAll(".dropdown-menu").forEach((menu) => {
+        menu.style.display = "none";
+      });
+      document.querySelectorAll(".dropdown-toggle").forEach((button) => {
+        button.classList.remove("active");
+      });
+      // Show or hide this dropdown
+      dropdownMenu.style.display = isShown ? "none" : "block";
+      toggleButton.classList.toggle("active", !isShown);
     });
+  }
 
-    // Add event listeners for other interactive elements as needed
-    var yearRange = document.getElementById('yearRange');
-    var fromYear = document.getElementById('fromYear');
-    var toYear = document.getElementById('toYear');
+  // Initialize all dropdowns
+  setupDropdown('yearToggleButton', 'yearDropdownMenu');
+  setupDropdown('authorToggleButton', 'authorDropdownMenu');
+  setupDropdown("documentToggleButton", "documentDropdownMenu");
+  setupDropdown("sourceToggleButton", "sourceDropdownMenu");
+  setupDropdown("affiliationToggleButton", "affiliationDropdownMenu");
+  setupDropdown("keyToggleButton", "keyDropdownMenu");
+  setupDropdown("languageToggleButton", "languageDropdownMenu");
 
-    yearRange.addEventListener('input', function() {
-        // Logic to update year range inputs
-        fromYear.value = this.value;
-        toYear.value = this.value; // Update this logic as per your requirement
-    });
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.getElementById('toggleButton');
-    const dropdownMenu = document.getElementById('dropdownMenu');
-    const rangeSelection = document.querySelector('.year-range-selection');
-    const individualSelection = document.querySelector('.individual-year-selection');
-    const rangeRadio = document.getElementById('range');
-    const individualRadio = document.getElementById('individual');
-
-    toggleButton.addEventListener('click', () => {
-        dropdownMenu.classList.toggle('show');
-    });
-
-    function toggleYearInput() {
-        if (rangeRadio.checked) {
-            rangeSelection.style.display = 'block';
-            individualSelection.style.display = 'none';
-        } else if (individualRadio.checked) {
-            rangeSelection.style.display = 'none';
-            individualSelection.style.display = 'block';
-        }
+  // Close dropdowns when clicking outside of them
+  window.addEventListener('click', (e) => {
+    if (!e.target.matches('.dropdown-toggle')) {
+        document.querySelectorAll('.dropdown-menu').forEach(menu => {
+            menu.style.display = 'none';
+        });
+        document.querySelectorAll('.dropdown-toggle').forEach(button => {
+            button.classList.remove('active');
+        });
     }
-
-    rangeRadio.addEventListener('change', toggleYearInput);
-    individualRadio.addEventListener('change', toggleYearInput);
-    
-    // Initial check
-    toggleYearInput();
+}, true);
 });
-
